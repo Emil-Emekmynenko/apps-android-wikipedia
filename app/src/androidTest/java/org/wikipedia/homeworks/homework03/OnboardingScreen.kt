053fc83c7edefed33717e875e06ed598ef0889e7
eemelyanenko <magvay7@mail.ru>
2025-02-06 10:57:10 +0300

    commit homework03

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework03/OnboardingScreen.kt
@@ -0,0 +1,64 @@
+package org.wikipedia.homeworks.homework03
+
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.tabs.TabLayout
+import org.wikipedia.views.AppTextView
+
+val skipButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_skip_button",
+    "onboarding_skip"
+)
+
+val continueButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_forward_button",
+    "onboarding_skip"
+)
+
+val addLanguageButton = listOf(
+    MaterialButton::class.java,
+    "addLanguageButton",
+    "onboarding_multilingual_add_language_text"
+)
+
+
+val tabLayoutButton = listOf(
+    TabLayout::class.java,
+    "view_onboarding_page_indicator"
+)
+
+
+val imageViewCentered = listOf(
+    LinearLayout::class.java,
+    "imageViewCentered"
+)
+
+
+val languagesList = listOf(
+    RecyclerView::class.java,
+    "languagesList"
+)
+
+
+val languagesListText = listOf(
+    AppTextView::class.java,
+    "option_label"
+)
+
+
+val startText = listOf(
+    AppTextView::class.java,
+    "secondaryTextView",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+)
+
+
+val startText2 = listOf(
+    AppTextView::class.java,
+    "primaryTextView",
+    "Lorem ipsum"
+)
+
