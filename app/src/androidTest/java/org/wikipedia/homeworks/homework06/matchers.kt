8126149b69baefc847fae36699b2f4a3d8272d8c
eemelyanenko <magvay7@mail.ru>
2025-02-16 23:11:43 +0300

    commit homework05 and homework06

--- app/src/androidTest/java/org/wikipedia/homeworks/homework06/matchers.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework06/matchers.kt
@@ -1,6 +1,8 @@
 package org.wikipedia.homeworks.homework06
 
 import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 
 enum class Color {
@@ -17,9 +19,11 @@ data class Shape(
     val color: Color
 )
 
+// Проверка длины стороны в заданном диапазоне
+
 class CheckSideLengthInRangeMatcher (
-    private val min: Double,
-    private val max: Double
+    private val min: Float,
+    private val max: Float
 ) : TypeSafeMatcher<Shape>() {
     override fun describeTo(description: Description) {
         description.appendText("длинна стороны входит в пределах между $min и $max")
@@ -29,6 +33,8 @@ class CheckSideLengthInRangeMatcher (
         return item.sideLength in min..min
     }
 }
+
+// Проверка на чётное количество сторон.
 
 class CheckingEvenNumberOfSides : TypeSafeMatcher<Shape>() {
     override fun describeTo(description: Description) {
@@ -40,7 +46,9 @@ class CheckingEvenNumberOfSides : TypeSafeMatcher<Shape>() {
     }
 }
 
-class CheckingColorOfShape(val checkColor : Color): TypeSafeMatcher<Shape>() {
+// Проверка цвета фигуры.
+
+class CheckingColorOfShape(private val checkColor : Color): TypeSafeMatcher<Shape>() {
     override fun describeTo(description: Description) {
         description.appendText("Цвет фигуры совпадает с $checkColor ")
     }
@@ -50,3 +58,88 @@ class CheckingColorOfShape(val checkColor : Color): TypeSafeMatcher<Shape>() {
     }
 
 }
+
+// Проверка на наличие отрицательной длины стороны (недопустимо).
+
+class NegativeSideLength: TypeSafeMatcher<Shape>() {
+    override fun describeTo(description: Description) {
+        description.appendText("Недопустимо, чтобы количество было отрицательным")
+    }
+
+    override fun matchesSafely(item: Shape): Boolean {
+        return item.sideLength <= 0.1
+    }
+}
+
+// Проверка на наличие отрицательного количества сторон (недопустимо)
+
+class NegativeNumberOfSides: TypeSafeMatcher<Shape>() {
+    override fun describeTo(description: Description) {
+        description.appendText("Недопустимо, чтобы количество было отрицательным")
+    }
+
+    override fun matchesSafely(item: Shape): Boolean {
+        return item.numberSides <= 0
+    }
+}
+
+// Проверка количества углов:
+
+class NumberOfAngles: TypeSafeMatcher<Shape>() {
+    override fun describeTo(description: Description) {
+        description.appendText("Количество углов")
+    }
+
+    override fun matchesSafely(item: Shape): Boolean {
+        return if (item.numberSides >= 3) {
+            true
+        } else item.numberSides in 0..2
+    }
+}
+
+class MatcherBuilder() {
+    val listOfMatchers = mutableListOf<Matcher<Shape>>()
+    operator fun invoke(function: MatcherBuilder.() -> Unit) {
+        function()
+    }
+
+}
+
+fun main() {
+    val shapes = listOf(
+        Shape(10f, 3, Color.RED),
+        Shape(5f, 4, Color.BLUE),
+        Shape(7f, 2, Color.GREEN),
+        Shape(0.5f, 1, Color.YELLOW),
+        Shape(-3f, 5, Color.BLACK),
+        Shape(8f, -2, Color.WHITE),
+        Shape(12f, 6, Color.RED),
+        Shape(15f, 8, Color.BLUE),
+        Shape(20f, 4, Color.GREEN),
+        Shape(9f, 5, Color.YELLOW),
+        Shape(2f, 3, Color.BLACK),
+        Shape(11f, 7, Color.WHITE),
+        Shape(6f, 10, Color.RED),
+        Shape(3f, 2, Color.BLUE),
+        Shape(4f, 1, Color.GREEN),
+        Shape(25f, 12, Color.YELLOW),
+        Shape(30f, 14, Color.BLACK),
+        Shape(35f, 16, Color.WHITE),
+        Shape(40f, 18, Color.RED),
+        Shape(50f, 20, Color.BLUE)
+    )
+    val matchers = allOf(
+        CheckSideLengthInRangeMatcher(1.0f,20.2f),
+        CheckingColorOfShape(Color.BLUE),
+        CheckingEvenNumberOfSides(),
+        NegativeNumberOfSides(),
+        NegativeSideLength(),
+        NumberOfAngles()
+    )
+
+    val result = shapes.filter {matchers.matches(it)}
+
+    println(result)
+}
+
+
