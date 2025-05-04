d1f04b97d1e700aef4114e2477229b775eba1555
eemelyanenko <magvay7@mail.ru>
2025-05-04 19:40:16 +0300

    commit homework26

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework26/CloseSyncBannerSmartScenario.kt
@@ -0,0 +1,16 @@
+package org.wikipedia.homeworks.homework26
+
+import com.kaspersky.kaspresso.testcases.core.testcontext.TestContext
+import com.kaspersky.kaspresso.testcases.models.info.StepInfo
+import io.github.kakaocup.kakao.common.views.KView
+
+class CloseSyncBannerSmartScenario(testContext: TestContext<*>) : BaseSmartScenario(testContext) {
+    override val stepInfo: String = "Закрывает окно с предложением синхронизации списка чтения"
+    override val action: (StepInfo) -> Unit = {
+        KView {
+            withId(android.R.id.button2)
+        }.click()
+    }
+
+    override fun isConditionMet() = waitElementByText("reading list")
+}
